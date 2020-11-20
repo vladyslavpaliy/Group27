856444368a95f02ecd9b9e83fa5aca3f39617b1b
altyshaibraim <altyshaibraim@gmail.com>
2020-11-20 00:32:06 -0600

    edited the file to see the changes

--- src/AltyshaIbraim/Task001.java
+++ src/AltyshaIbraim/Task001.java
@@ -3,5 +3,6 @@ package AltyshaIbraim;
 public class Task001 {
     public static void main(String[] args) {
         System.out.println("Thank you All!");
+        System.out.println("Check Check");
     }
 }
